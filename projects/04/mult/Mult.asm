3ffb9de91e9e803c03a49db0a588a89fe849a7bb
mopp <hello@mopp.jp>
2019-12-08 20:07:35 +0900

    project04: write Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,28 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @R2
+    M = 0
+(LOOP)
+    // Jump to END if the R1 is zero.
+    @R1
+    D = M
+    @END
+    D;JEQ
+
+    // Add R0 to R2.
+    @R0
+    D = M
+    @R2
+    M = M + D
+
+    // Decrement R1
+    @R1
+    M = M - 1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
