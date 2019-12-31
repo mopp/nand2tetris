304cba511902cbf0bc93d3223728dc3b00e63961
mopp <hello@mopp.jp>
2019-12-31 20:52:50 +0900

    project06: implement code::comp

--- projects/06/assembler/src/code.rs
+++ projects/06/assembler/src/code.rs
@@ -12,6 +12,31 @@ fn dest(mnemonic: Option<String>) -> String {
     }
 }
 
+fn comp(mnemonic: String) -> String {
+    match mnemonic.as_str() {
+        "0" => "101010",
+        "1" => "111111",
+        "-1" => "111010",
+        "D" => "001100",
+        "A" | "M" => "110000",
+        "!D" => "001101",
+        "!A" | "!M" => "110001",
+        "-D" => "001111",
+        "-A" | "-M" => "110011",
+        "D+1" => "011111",
+        "A+1" | "M+1" => "110111",
+        "D-1" => "001110",
+        "A-1" | "M-1" => "110010",
+        "D+A" | "D+M" => "000010",
+        "D-A" | "D-M" => "010011",
+        "A-D" | "M-D" => "000111",
+        "D&A" | "D&M" => "000000",
+        "D|A" | "D|M" => "010101",
+        _ => panic!("unexpected mnemonic was given: {:?}", mnemonic),
+    }
+    .to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
